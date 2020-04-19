319f72fc35201f75264a5ce54740250eaceb02ad
j-rockwell <jrobertrockwell@gmail.com>
2020-04-19 14:02:01 -0700

    Added chat cooldowns

--- src/main/java/com/llewkcor/ares/humbug/cont/mods/ChatMod.java
+++ src/main/java/com/llewkcor/ares/humbug/cont/mods/ChatMod.java
@@ -1,7 +1,10 @@
 package com.llewkcor.ares.humbug.cont.mods;
 
+import com.google.common.collect.Maps;
 import com.llewkcor.ares.commons.event.ProcessedChatEvent;
+import com.llewkcor.ares.commons.util.bukkit.Scheduler;
 import com.llewkcor.ares.commons.util.general.Configs;
+import com.llewkcor.ares.commons.util.general.Time;
 import com.llewkcor.ares.humbug.Humbug;
 import com.llewkcor.ares.humbug.cont.HumbugMod;
 import lombok.Getter;
@@ -20,19 +23,26 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 public final class ChatMod implements HumbugMod, Listener {
     @Getter public final Humbug plugin;
     @Getter public final String name = "Chat";
     @Getter @Setter public boolean enabled;
 
+    @Getter public final Map<UUID, Long> recentChatters;
+
     @Getter public boolean hideJoinLeaveMessages;
     @Getter public boolean disablePostingLinks;
     @Getter public List<String> whitelistedLinks;
+    @Getter public boolean rateLimitChatEnabled;
+    @Getter public int chatRateLimit;
 
     public ChatMod(Humbug plugin) {
         this.plugin = plugin;
         this.enabled = false;
+        this.recentChatters = Maps.newConcurrentMap();
 
         Bukkit.getPluginManager().registerEvents(this, plugin);
     }
@@ -44,6 +54,8 @@ public final class ChatMod implements HumbugMod, Listener {
         this.hideJoinLeaveMessages = config.getBoolean("mods.chat.hide_join_leave_messages");
         this.disablePostingLinks = config.getBoolean("mods.chat.disable_posting_links");
         this.whitelistedLinks = config.getStringList("mods.chat.allowed_links");
+        this.rateLimitChatEnabled = config.getBoolean("mods.chat.chat_cooldowns.enabled");
+        this.chatRateLimit = config.getInt("mods.chat.rate_limit");
 
         this.enabled = true;
     }
@@ -51,6 +63,15 @@ public final class ChatMod implements HumbugMod, Listener {
     @Override
     public void unload() {
         this.enabled = false;
+    }
+
+    /**
+     * Returns a time in milliseconds when the player can chat again
+     * @param player Player
+     * @return Time in ms
+     */
+    private long getRemainingChatCooldown(Player player) {
+        return recentChatters.getOrDefault(player.getUniqueId(), 0L);
     }
 
     /**
@@ -113,7 +134,7 @@ public final class ChatMod implements HumbugMod, Listener {
     }
 
     @EventHandler (priority = EventPriority.HIGHEST)
-    public void onChat(ProcessedChatEvent event) {
+    public void onPostLink(ProcessedChatEvent event) {
         if (!isEnabled() || !isDisablePostingLinks() || event.isCancelled()) {
             return;
         }
@@ -125,12 +146,41 @@ public final class ChatMod implements HumbugMod, Listener {
         for (String str : split) {
             if (isBlacklistedLink(str)) {
 
-                player.sendMessage(ChatColor.RED + "This type of link is blacklisted for non-premium users. Purchase a rank at " + ChatColor.AQUA +
-                        "https://playares.com/store" + ChatColor.RED + " to bypass this restriction");
+                player.sendMessage(ChatColor.RED + "This type of link is blacklisted for non-premium users.");
+                player.sendMessage(ChatColor.YELLOW + "To bypass this filter purchase a premium rank at " + ChatColor.AQUA + "https://playares.com/store");
 
                 event.setCancelled(true);
                 return;
             }
         }
+    }
+
+    @EventHandler (priority = EventPriority.MONITOR)
+    public void onChat(ProcessedChatEvent event) {
+        if (!isEnabled() || !isRateLimitChatEnabled()) {
+            return;
+        }
+
+        final Player player = event.getPlayer();
+
+        if (player.hasPermission("humbug.chat.bypass")) {
+            return;
+        }
+
+        final long remainingCooldown = getRemainingChatCooldown(player);
+
+        if (remainingCooldown > Time.now()) {
+            player.sendMessage(ChatColor.RED + "Please wait " + (Time.convertToDecimal(remainingCooldown - Time.now()) + "s before sending another message."));
+            player.sendMessage(ChatColor.YELLOW + "To bypass this filter purchase a premium rank at " + ChatColor.AQUA + "https://playares.com/store");
+
+            event.setCancelled(true);
+            return;
+        }
+
+        final UUID uniqueId = player.getUniqueId();
+        final long nextAllowedMessage = (Time.now() + (chatRateLimit * 1000L));
+
+        recentChatters.put(player.getUniqueId(), nextAllowedMessage);
+        new Scheduler(plugin).async(() -> recentChatters.remove(uniqueId)).delay(chatRateLimit * 20L).run();
     }
 }
